a63efa6343d633c714eaddd96308bb4f03c2cc53
Meir J Cohen <meir84@gmail.com>
2022-12-06 01:28:02 +0200

    added assigments

--- 220929/Q12.3/script.js
+++ 220929/Q12.3/script.js
@@ -1,8 +1,91 @@
-const toys = [{name:"yo-yo",color:"red"},{name:"doll",color:"blue"},{name:"bow",color:"brown"},{name:"barbi",color:"pink"}]
+// const toys = [{name:"yo-yo",color:"red"},{name:"doll",color:"blue"},{name:"bow",color:"brown"},{name:"barbi",color:"pink"}]
 
-const aToys = toys.filter(toy => toy.name.startsWith("b"))
-console.log(aToys)
+// const aToys = toys.filter(toy => toy.name.startsWith("b"))
+// console.log(aToys)
 
-const isColorBlue = toys.filter(toy => toy.color.includes("blue"))
-console.log(isColorBlue)
-isColorBlue.length > 0 ? console.log(true):console.log(false);
+// const isColorBlue = toys.filter(toy => toy.color.includes("blue"))
+// console.log(isColorBlue)
+// isColorBlue.length > 0 ? console.log(true):console.log(false);
+
+const school = {
+  teachers: [
+    {
+      id: 1,
+      name: "Pinchas",
+      subjects: ["chemistry", "biology", "physics"],
+      students: [],
+      capacityLeft: 3,
+    },
+    {
+      id: 2,
+      name: "Williams",
+      subjects: ["history", "ethics"],
+      students: [],
+      capacityLeft: 2,
+    },
+  ],
+  students: [
+    {
+      id: 10,
+      name: "Jennifer",
+      age: 20,
+    },
+    {
+      id: 11,
+      name: "Howard",
+      age: 23,
+    },
+    {
+      id: 12,
+      name: "Old-Timmy",
+      age: 86,
+    },
+    {
+      id: 13,
+      name: "Houston",
+      age: 21,
+    },
+  ],
+};
+
+//? 1
+school.findPerson = function (type, id) {
+  const students = this[type];
+  const person = students.find((e, i) => {
+    return e.id === id;
+  });
+  return person;
+};
+// console.log(school.findPerson('teachers', 2));
+
+//? 2
+school.assignStudent = function (id, subject) {
+  const student = school.findPerson("students", id);
+  if (!student) {
+    return "No student with the specified id was found";
+  }
+  const teacher = school.teachers.find((e) => {
+    return e.subjects.includes(subject);
+  });
+
+  if (teacher.capacityLeft < 1) {
+    return "Sorry, no available teachers left";
+  } else {
+    teacher.students.push(student);
+    teacher.capacityLeft--;
+    return teacher;
+  }
+};
+// console.log(school.assignStudent(10, 'history'));
+
+school.assignTeachersSubject = function (id, subject) {
+  const teacher = school.teachers.find((e) => {
+    return e.id === id && !e.subjects.includes(subject.toLowerCase());
+  });
+  if (teacher) {
+    teacher.subjects.push(subject.toLowerCase());
+    return teacher;
+  }
+};
+
+console.log(school.assignTeachersSubject(2, "Math"));
